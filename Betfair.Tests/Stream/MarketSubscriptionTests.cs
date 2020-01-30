ca14b0880af89aa3cc339eec76b092e2ac3ec718
KelvinVail <26182999+KelvinVail@users.noreply.github.com>
2020-01-30 15:23:28 +0000

    Add Connection Message to MarketSubcription

--- Betfair.Tests/Stream/MarketSubscriptionTests.cs
+++ Betfair.Tests/Stream/MarketSubscriptionTests.cs
@@ -1,8 +1,12 @@
-﻿namespace Betfair.Tests.Stream
+﻿using System.Diagnostics;
+
+namespace Betfair.Tests.Stream
 {
     using System;
+    using System.IO;
     using System.Net.Security;
-    using System.Net.Sockets;
+    using System.Text;
+    using System.Threading.Tasks;
     using Betfair.Streaming;
     using Betfair.Tests.TestDoubles;
     using Xunit;
@@ -12,6 +16,7 @@
         private readonly SessionSpy session = new SessionSpy();
         private readonly TcpClientSpy client;
         private readonly MarketSubscription marketSubscription;
+        private readonly StringBuilder sendLines = new StringBuilder();
         private bool disposed;
 
         public MarketSubscriptionTests()
@@ -35,59 +40,104 @@
         }
 
         [Fact]
-        public void WhenInitializedReceiveBufferSizeIsSet()
+        public void OnConnectReceiveBufferSizeIsSet()
         {
+            this.marketSubscription.Connect();
             Assert.Equal(1024 * 1000 * 2, this.client.ReceiveBufferSize);
         }
 
         [Fact]
-        public void WhenInitializedReaderWriteTimeoutIsThirtySeconds()
+        public void OnConnectReaderWriteTimeoutIsThirtySeconds()
         {
+            this.marketSubscription.Connect();
             Assert.Equal(TimeSpan.FromSeconds(30).TotalMilliseconds, this.marketSubscription.Reader.BaseStream.WriteTimeout);
         }
 
         [Fact]
-        public void WhenInitializedReaderReadTimeoutIsThirtySeconds()
+        public void OnConnectReaderReadTimeoutIsThirtySeconds()
         {
+            this.marketSubscription.Connect();
             Assert.Equal(TimeSpan.FromSeconds(30).TotalMilliseconds, this.marketSubscription.Reader.BaseStream.ReadTimeout);
         }
 
         [Fact]
-        public void WhenInitializedWriterWriteTimeoutIsThirtySeconds()
+        public void OnConnectWriterWriteTimeoutIsThirtySeconds()
         {
+            this.marketSubscription.Connect();
             Assert.Equal(TimeSpan.FromSeconds(30).TotalMilliseconds, this.marketSubscription.Writer.BaseStream.WriteTimeout);
         }
 
         [Fact]
-        public void WhenInitializedWriterReadTimeoutIsThirtySeconds()
+        public void OnConnectWriterReadTimeoutIsThirtySeconds()
         {
+            this.marketSubscription.Connect();
             Assert.Equal(TimeSpan.FromSeconds(30).TotalMilliseconds, this.marketSubscription.Writer.BaseStream.ReadTimeout);
         }
 
         [Fact]
-        public void WhenInitializedTcpClientIsConnectedToBetfairHost()
+        public void OnConnectTcpClientIsConnectedToBetfairHost()
         {
+            this.marketSubscription.Connect();
             Assert.Equal("stream-api.betfair.com", this.client.Host);
         }
 
         [Fact]
-        public void WhenInitializedTcpClientIsConnectedToBetfairPort()
+        public void OnConnectTcpClientIsConnectedToBetfairPort()
         {
+            this.marketSubscription.Connect();
             Assert.Equal(443, this.client.Port);
         }
 
         [Fact]
-        public void WhenInitializedReaderStreamIsAuthenticated()
+        public void OnConnectReaderStreamIsAuthenticated()
         {
+            this.marketSubscription.Connect();
             var sslStream = (SslStream)this.marketSubscription.Reader.BaseStream;
             Assert.True(sslStream.IsAuthenticated);
         }
 
         [Fact]
-        public void WhenInitializedWriterStreamIsAuthenticated()
+        public void OnConnectWriterStreamIsAuthenticated()
         {
+            this.marketSubscription.Connect();
             var sslStream = (SslStream)this.marketSubscription.Writer.BaseStream;
             Assert.True(sslStream.IsAuthenticated);
+        }
+
+        [Fact]
+        public void OnConnectWriterIsAutoFlushed()
+        {
+            this.marketSubscription.Connect();
+            Assert.True(this.marketSubscription.Writer.AutoFlush);
+        }
+
+        [Theory]
+        [InlineData("106-300120115247-8647")]
+        [InlineData("ConnectionId")]
+        public async Task OnReadConnectionResponseConnectionStatusIsUpdated(string id)
+        {
+            this.SendLine($"{{\"op\":\"connection\",\"connectionId\":\"{id}\"}}");
+            await this.marketSubscription.Start();
+            Assert.Equal(id, this.marketSubscription.ConnectionId);
+        }
+
+        [Theory]
+        [InlineData("106-300120115247-8647")]
+        [InlineData("ConnectionId")]
+        [InlineData("ConnectionId2")]
+        [InlineData("ConnectionId3")]
+        [InlineData("ConnectionId4")]
+        public async Task Timer(string id)
+        {
+            var timer = new Stopwatch();
+            var ticksPerSecond = Stopwatch.Frequency;
+            this.SendLine($"{{\"op\":\"connection\",\"connectionId\":\"{id}\"}}");
+            timer.Start();
+            await this.marketSubscription.Start();
+            timer.Stop();
+            var ms = timer.ElapsedMilliseconds;
+            var t = timer.ElapsedTicks;
+            Assert.Equal(id, this.marketSubscription.ConnectionId);
         }
 
         public void Dispose()
@@ -107,6 +157,12 @@
 
                 this.disposed = true;
             }
+        }
+
+        private void SendLine(string line)
+        {
+            this.sendLines.AppendLine(line);
+            this.marketSubscription.Reader = new StreamReader(new MemoryStream(Encoding.UTF8.GetBytes(this.sendLines.ToString())));
         }
     }
 }
--- Betfair/Streaming/MarketSubscription.cs
+++ Betfair/Streaming/MarketSubscription.cs
@@ -3,28 +3,49 @@
     using System;
     using System.IO;
     using System.Net.Security;
+    using System.Net.Sockets;
     using System.Text;
+    using System.Threading.Tasks;
+    using Newtonsoft.Json;
 
     public sealed class MarketSubscription
     {
         private const string HostName = "stream-api.betfair.com";
         private readonly ISession session;
+        private ITcpClient tcpClient = new ExchangeStreamClient();
 
         public MarketSubscription(ISession session)
         {
             this.session = session;
         }
 
-        public StreamReader Reader { get; private set; }
+        public StreamReader Reader { get; set; }
 
         public StreamWriter Writer { get; private set; }
 
+        public string ConnectionId { get; private set; }
+
         public void WithTcpClient(ITcpClient client)
         {
+            this.tcpClient = client;
             if (client is null) throw new ArgumentNullException(nameof(client));
-            var stream = GetSslStream(client);
-            this.Reader = new StreamReader(stream, Encoding.UTF8, false, client.ReceiveBufferSize);
-            this.Writer = new StreamWriter(stream, Encoding.UTF8);
+        }
+
+        public void Connect()
+        {
+            var stream = GetSslStream(this.tcpClient);
+            this.Reader = new StreamReader(stream, Encoding.UTF8, false, this.tcpClient.ReceiveBufferSize);
+            this.Writer = new StreamWriter(stream, Encoding.UTF8) { AutoFlush = true };
+        }
+
+        public async Task Start()
+        {
+            while (true)
+            {
+                var line = await this.Reader.ReadLineAsync();
+                if (line == null) return;
+                this.ProcessLine(line);
+            }
         }
 
         private static SslStream GetSslStream(ITcpClient client)
@@ -38,6 +59,50 @@
             var sslStream = new SslStream(stream, false);
             sslStream.AuthenticateAsClient(HostName);
             return sslStream;
+        }
+
+        private static MessageOperation GetOperation(string line)
+        {
+            return JsonConvert.DeserializeObject<MessageOperation>(line);
+        }
+
+        private void ProcessLine(string line)
+        {
+            var operation = GetOperation(line);
+            if (operation.IsConnectionMessage)
+                this.ProcessConnectionMessage(line);
+        }
+
+        private void ProcessConnectionMessage(string line)
+        {
+            var connectionMessage = JsonConvert.DeserializeObject<ConnectionMessage>(line);
+            this.ConnectionId = connectionMessage.ConnectionId;
+        }
+
+        private sealed class ExchangeStreamClient : TcpClient, ITcpClient
+        {
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage(
+            "Microsoft.Performance",
+            "CA1812:AvoidUninstantiatedInternalClasses",
+            Justification = "Used to deserialize Json.")]
+        private sealed class MessageOperation
+        {
+            [JsonProperty(PropertyName = "op")]
+            internal string Operation { get; set; }
+
+            internal bool IsConnectionMessage => this.Operation == "connection";
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage(
+            "Microsoft.Performance",
+            "CA1812:AvoidUninstantiatedInternalClasses",
+            Justification = "Used to deserialize Json.")]
+        private sealed class ConnectionMessage
+        {
+            [JsonProperty(PropertyName = "connectionId")]
+            internal string ConnectionId { get; set; }
         }
     }
 }
