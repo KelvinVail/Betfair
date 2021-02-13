f297797a3c26623c37ed5f3cd7a00c430f74bceb
KelvinVail <26182999+KelvinVail@users.noreply.github.com>
2021-02-13 18:41:44 +0000

    Add BetfairHttpClient

---
+++ src/Betfair.Service.Tests/BetfairHttpClientTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net.Http.Headers;
+using Xunit;
+
+namespace Betfair.Service.Tests
+{
+    public class BetfairHttpClientTests : BetfairHttpClient
+    {
+        [Fact]
+        public void TimeoutIsSetToThirtySeconds()
+        {
+            Assert.Equal(TimeSpan.FromSeconds(30), Timeout);
+        }
+
+        [Fact]
+        public void AcceptHeaderContainsApplicationJson()
+        {
+            Assert.Contains(
+                new MediaTypeWithQualityHeaderValue("application/json"),
+                DefaultRequestHeaders.Accept);
+        }
+
+        [Fact]
+        public void ConnectionIsSetToKeepAlive()
+        {
+            Assert.Equal("keep-alive", DefaultRequestHeaders.Connection.ToString());
+        }
+
+        [Fact]
+        public void AcceptGzipEncoding()
+        {
+            Assert.Contains(
+                "gzip",
+                DefaultRequestHeaders.AcceptEncoding.ToString(),
+                StringComparison.InvariantCulture);
+        }
+
+        [Fact]
+        public void AcceptDeflateEncoding()
+        {
+            Assert.Contains(
+                "deflate",
+                DefaultRequestHeaders.AcceptEncoding.ToString(),
+                StringComparison.InvariantCulture);
+        }
+    }
+}
