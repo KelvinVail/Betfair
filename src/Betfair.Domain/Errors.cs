c654b32acfa7653043e1fe350878f9b2bd71ea59
KelvinVail <26182999+KelvinVail@users.noreply.github.com>
2022-11-12 11:20:29 +0000

    Clean start

--- src/Betfair.Domain/Errors.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Betfair.Domain
-{
-    public static class Errors
-    {
-        public static DomainError InvalidPrice(decimal price) =>
-            new ("invalid.price", $"'{price}' is an invalid price.");
-    }
-}
--- src/Betfair.Exchange.Interfaces/IExchangeService.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace Betfair.Exchange.Interfaces
-{
-    public interface IExchangeService
-    {
-        Task<T> SendAsync<T>(string endpoint, string betfairMethod, string parameters);
-    }
-}
