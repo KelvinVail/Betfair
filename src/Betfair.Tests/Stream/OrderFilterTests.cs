5d2cda2bc41008e5b80d440c01e201f96b3f5090
Kelvin Vail <26182999+KelvinVail@users.noreply.github.com>
2024-02-03 15:54:07 +0000

    Add OrderFilter

---
+++ src/Betfair.Tests/Stream/OrderFilterTests.cs
@@ -0,0 +1,54 @@
+﻿using Betfair.Stream;
+
+namespace Betfair.Tests.Stream;
+
+public class OrderFilterTests
+{
+    private readonly OrderFilter _filter = new ();
+
+    [Fact]
+    public void IncludeOverallPositionIsNullAsDefault() =>
+        _filter.IncludeOverallPosition.Should().BeNull();
+
+    [Fact]
+    public void PartitionMatchedByStrategyRefIsNullAsDefault() =>
+        _filter.PartitionMatchedByStrategyRef.Should().BeNull();
+
+    [Fact]
+    public void CustomerStrategyRefsIsNullAsDefault() =>
+        _filter.CustomerStrategyRefs.Should().BeNull();
+
+    [Fact]
+    public void WithAggregatedPositionsSetsOverallPositionToTrue()
+    {
+        _filter.WithAggregatedPositions();
+
+        _filter.IncludeOverallPosition.Should().BeTrue();
+    }
+
+    [Fact]
+    public void WithDetailedPositionsSetsOverallPositionToFalse()
+    {
+        _filter.WithDetailedPositions();
+
+        _filter.IncludeOverallPosition.Should().BeFalse();
+    }
+
+    [Fact]
+    public void WithOrderPerStrategySetsPartitionToTrue()
+    {
+        _filter.WithOrdersPerStrategy();
+
+        _filter.PartitionMatchedByStrategyRef.Should().BeTrue();
+    }
+
+    [Theory]
+    [InlineData("Ref001")]
+    [InlineData("experimental")]
+    public void StrategyRefAreAddedToFilter(string strategyRef)
+    {
+        _filter.WithStrategyRefs(strategyRef);
+
+        _filter.CustomerStrategyRefs.Should().Contain(strategyRef);
+    }
+}
---
+++ src/Betfair/Stream/OrderFilter.cs
@@ -0,0 +1,61 @@
+﻿namespace Betfair.Stream;
+
+public class OrderFilter
+{
+    private HashSet<string>? _strategyRefs;
+
+    public bool? IncludeOverallPosition { get; private set; } = null;
+
+    public bool? PartitionMatchedByStrategyRef { get; private set; } = null;
+
+    public IReadOnlyCollection<string>? CustomerStrategyRefs => _strategyRefs;
+
+    /// <summary>
+    /// Returns aggregated order data per runner. e.g. (Back / Lays).
+    /// </summary>
+    /// <returns>This <see cref="OrderFilter"/>.</returns>
+    public OrderFilter WithAggregatedPositions()
+    {
+        IncludeOverallPosition = true;
+        return this;
+    }
+
+    /// <summary>
+    /// Returns data for each individual order per runner.
+    /// </summary>
+    /// <returns>This <see cref="OrderFilter"/>.</returns>
+    public OrderFilter WithDetailedPositions()
+    {
+        IncludeOverallPosition = false;
+        return this;
+    }
+
+    /// <summary>
+    /// Returns orders aggregated per runner, per strategy reference
+    /// if aggregated positions have been request.
+    /// Has no effect if detailed permissions have been requested.
+    /// </summary>
+    /// <returns>This <see cref="OrderFilter"/>.</returns>
+    public OrderFilter WithOrdersPerStrategy()
+    {
+        PartitionMatchedByStrategyRef = true;
+        return this;
+    }
+
+    /// <summary>
+    /// Restricts to specified customerStrategyRefs (specified in placeOrders).
+    /// This will filter orders and StrategyMatchChanges accordingly
+    /// (Note: overall position is not filtered)
+    /// </summary>
+    /// <param name="strategyRefs">A set of strategy references (specified in placeOrders).</param>
+    /// <returns>This <see cref="OrderFilter"/>.</returns>
+    public OrderFilter WithStrategyRefs(params string[] strategyRefs)
+    {
+        _strategyRefs ??=[];
+
+        foreach (var strategyRef in strategyRefs.Where(x => x is not null))
+            _strategyRefs.Add(strategyRef);
+
+        return this;
+    }
+}
