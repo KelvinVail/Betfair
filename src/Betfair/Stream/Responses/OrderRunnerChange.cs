37d8c1e7b53d9e7c5363db294262a075ce517762
Kelvin Vail <26182999+KelvinVail@users.noreply.github.com>
2024-06-16 15:01:31 +0100

    Update nullability of OrderRunnerChage values

--- src/Betfair/Stream/Responses/OrderRunnerChange.cs
+++ src/Betfair/Stream/Responses/OrderRunnerChange.cs
@@ -3,10 +3,10 @@ namespace Betfair.Stream.Responses;
 public class OrderRunnerChange
 {
     [JsonPropertyName("mb")]
-    public List<List<double?>>? MatchedBacks { get; init; }
+    public List<List<double>>? MatchedBacks { get; init; }
 
     [JsonPropertyName("id")]
-    public long? SelectionId { get; init; }
+    public long SelectionId { get; init; }
 
     [JsonPropertyName("hc")]
     public double? Handicap { get; init; }
@@ -15,7 +15,7 @@ public class OrderRunnerChange
     public bool? FullImage { get; init; }
 
     [JsonPropertyName("ml")]
-    public List<List<double?>>? MatchedLays { get; init; }
+    public List<List<double>>? MatchedLays { get; init; }
 
     [JsonPropertyName("uo")]
     public List<UnmatchedOrder>? UnmatchedOrders { get; init; }
